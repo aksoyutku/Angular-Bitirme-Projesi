a728bd82906fb933dc44d0be0752cf4c5130fda4
nevergr33n <exilesama@gmail.com>
2022-08-12 22:34:42 +0300

    category update

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -8,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class CategoriesComponent implements OnInit {
   constructor() {}
 
+  token = localStorage.getItem('token');
   categories: any = [];
 
   async getCategories() {
-    const res = await fetch('http://localhost:3030/api/categories');
+    const res = await fetch('http://localhost:3030/api/categories', {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + this.token,
+        Host: 'api.producthunt.com',
+      },
+    });
     const data: [] = await res.json();
     this.categories = [...data];
     console.log(this.categories);
